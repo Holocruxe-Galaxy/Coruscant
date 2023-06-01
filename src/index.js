4b0d7f7fd488ea56fa8be90dd5ca93871477504f
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2023-06-01 12:33:29 -0300

    .env

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const server = require("./app");
 const {conn} = require("./db");
 
 server.listen(3000, () => {
-  conn.sync({force: true});
+  conn.sync({force: false});
   console.log("server is listening on port 3000");
 });
