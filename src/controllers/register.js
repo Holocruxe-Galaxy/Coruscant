d3ba939dd59f4a5e1cb89b405756298886754d1c
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2023-06-03 16:55:49 -0300

    agregando validación form register#

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -2,15 +2,8 @@ const {User} = require("../db");
 
 const createUser = async (req, res) => {
   try {
-    const {
-      username,
-      name,
-      lastname,
-      email,
-      image_profile_url,
-
-      birthdate,
-    } = req.body;
+    const {username, name, lastname, email, image_profile_url, birthdate} =
+      req.body;
 
     const [user, created] = await User.findOrCreate({
       where: {email: email},
@@ -25,13 +18,24 @@ const createUser = async (req, res) => {
       },
     });
 
-    !created
-      ? res
-          .status(201)
-          .send(
-            "El correo electrónico ya está asociado a una cuenta, intente iniciar sesión"
-          )
-      : res.status(200).send("El usuario fué creado correctamente");
+    if (
+      !username ||
+      !name ||
+      !lastname ||
+      !email ||
+      !image_profile_url ||
+      !birthdate
+    ) {
+      res.status(201).send("Verifique si falta la información de algún campo");
+    } else {
+      !created
+        ? res
+            .status(201)
+            .send(
+              "El correo electrónico ya está asociado a una cuenta, intente iniciar sesión"
+            )
+        : res.status(200).send("El usuario fué creado correctamente");
+    }
   } catch (error) {
     console.log(error);
   }
