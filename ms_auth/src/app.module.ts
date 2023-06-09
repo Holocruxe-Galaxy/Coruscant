08440a70a8248a25283dc254eaf20bfdab7f1234
JaluGJ <gabriel.i.jalil@gmail.com>
2023-06-09 11:30:27 -0300

    add(app.module): db loger

--- ms_auth/src/app.module.ts
+++ ms_auth/src/app.module.ts
@@ -4,13 +4,14 @@ import { UsersModule } from './users/users.module';
 import * as dotenv from 'dotenv';
 import { ConfigModule } from '@nestjs/config';
 dotenv.config();
-const { DB_HOST, DB_USER, DB_NAME, DB_PASSWORD } = process.env;
+const { DB_HOST, DB_USER, DB_NAME, DB_PASSWORD, PRODUCTION } = process.env;
 console.log(DB_HOST);
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      logging: !Boolean(PRODUCTION),
       type: 'postgres',
       host: DB_HOST,
       port: 5432,
