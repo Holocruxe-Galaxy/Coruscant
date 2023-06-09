c2bb763b909757006e770d984b45fc3f15729d4a
JaluGJ <gabriel.i.jalil@gmail.com>
2023-06-09 11:04:38 -0300

    bug(config): creating configurations to run

--- ms_auth/src/app.module.ts
+++ ms_auth/src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import dotenv from 'dotenv';
-dotenv.config();
-const { DB_HOST, DB_USER, DB_NAME, DB_PASSWORD } = process.env;
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+/* import dotenv from 'dotenv';
+dotenv.config(); */
+const { DB_HOST, DB_USER, DB_NAME, DB_PASSWORD, PRODUCTION } = process.env;
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: DB_HOST,
@@ -16,8 +19,9 @@ const { DB_HOST, DB_USER, DB_NAME, DB_PASSWORD } = process.env;
       password: DB_PASSWORD,
       database: DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+      synchronize: !Boolean(PRODUCTION),
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
