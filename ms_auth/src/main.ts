3a529da4f412549f9879002cbb02d3d1e972b876
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2023-06-25 22:28:46 -0300

    add cors

--- ms_auth/src/main.ts
+++ ms_auth/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'Content-Type, Accept',
+    }),
+  );
   await app.listen(process.env.PORT);
   console.log(`Server running on ${process.env.PORT}`);
 }
