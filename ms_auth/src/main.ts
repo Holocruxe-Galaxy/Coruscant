ab03fff3821ca4f9af84fadf41aca94c321981e6
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2023-10-25 14:26:26 -0300

    probando si se arregla un bug: cambiando  cors

--- ms_auth/src/main.ts
+++ ms_auth/src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    credentials: true,
-  });
+  app.use(
+    cors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'Content-Type, Accept',
+      exposedHeaders: ['Authorization'],
+    }),
+  );
 
   await app.listen(process.env.PORT);
   console.log(`Server running on ${process.env.PORT}`);
