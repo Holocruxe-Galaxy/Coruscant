a059394a15aadcfd2cbbb69cd6352dd4c0912fc3
chaque1111 <minemax378@gmail.com>
2024-02-22 16:00:45 -0300

    fix: adding new fields to the user entity, and modifying app.cors()

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cors({
-      origin: '*',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      allowedHeaders: 'Content-Type, Accept',
-      exposedHeaders: ['Authorization'],
-    }),
-  );
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    exposedHeaders: ['Authorization'],
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT);
   console.log(`Server running on ${process.env.PORT}`);
